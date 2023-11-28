5a7ff948ea415da2523123b16c7bfc4c90dcf2ef
EricEZz <zhangeric343@gmail.com>
2023-11-28 10:12:13 -0500

    d

--- project/js/Dom.js
+++ project/js/Dom.js
@@ -2,6 +2,8 @@ const cameras = [
     {
         name:"Blink Video Doorbell",
         brand:"Blink",
+        smart: true,
+        wireless: false,
         img:"https://m.media-amazon.com/images/I/41te-uzWxDL._AC_SY450_.jpg",
         price:"29.99",
         desc:"Two-way audio, HD video, motion and chime app alerts and Alexa enabled — wired or wire-free (Black)"
@@ -9,6 +11,7 @@ const cameras = [
     {
         name:"Blink Mini",
         brand:"Blink",
+        smart: false,
         img:"https://m.media-amazon.com/images/I/31k4JysPQ5L._SY450_.jpg",
         price:"19.99",
         desc:"Compact indoor plug-in smart security camera, 1080p HD video, night vision, motion detection, two-way audio, easy set up, Works with Alexa 1 camera (White)"
@@ -16,13 +19,16 @@ const cameras = [
     {
         name:"Blink Outdoor 4",
         brand:"Blink",
+        smart: false,
+        wireless: true,
         img:"https://m.media-amazon.com/images/I/41PtM0usvoL._SY450_.jpg",
         price:"134.99",
-        desc:"Wire-free smart security camera, two-year battery life, two-way audio, HD live view, enhanced motion detection, Works with Alexa – 3 camera system"
+        desc:"Wire-free smart security camera, two-year battery life, two-way audio, HD live view, enhanced motion detection, Works with Alexa 3 camera system"
     },
     {
         name:"Blink Mini Pan-Tilt Camera",
         brand:"Blink",
+        smart: true,
         img:"https://m.media-amazon.com/images/I/41aicdAQHVL._SY450_.jpg",
         price:"29.99",
         desc:"Rotating indoor plug-in smart security camera, two-way audio, HD video, motion detection, Works with Alexa (White)"
@@ -30,6 +36,8 @@ const cameras = [
     {
         name:"blurams Security Camera",
         brand: "Blurams",
+        smart: false,
+        wireless: false,
         img:"https://m.media-amazon.com/images/I/61UMxAUOHWL._AC_SX466_.jpg",
         price:"23.10",
         desc:" 2K Indoor Camera 360° Pet Camera for Home Security w/Motion Tracking, Phone App, 2-Way Audio, IR Night Vision, Siren, Works with Alexa & Google Assistant(2.4GHz ONLY)"
@@ -44,6 +52,7 @@ const cameras = [
     {
         name:"EKEN Video Doorbell Camera",
         brand:"EKEN",
+        smart: false,
         img:"https://m.media-amazon.com/images/I/61hfszB57lL._AC_SX466_.jpg",
         price:"29.59",
         desc:"Wireless with Chime Ringer, Smart AI Human Detection, 2.4G WiFi, 2-Way Audio, HD Live Image, Night Vision, Cloud Storage, Battery Powered, Indoor/Outdoor Surveillance",
@@ -51,6 +60,8 @@ const cameras = [
     {
         name:"blurams Outdoor Security Camera",
         Brand:"Blurams",
+        smart: true,
+        wireless: false,
         img:"https://m.media-amazon.com/images/I/51qqzYWF+ML._AC_SX466_.jpg",
         price:"41.68",
         desc:"Security Camera 3MP, Wi-Fi Outside Cameras for Home Security w/Smart Motion Detection and Spotlight Siren Alarm, Compatible with Alexa & Google Assistant, (2 Pack)",
@@ -58,13 +69,35 @@ const cameras = [
     {
         name:"blurams Security Camera Outdoor",
         Brand:"Blurams",
+        smart: true,
+        wireless: true,
         img:"https://m.media-amazon.com/images/I/61+pRFG0QTL._AC_SX466_.jpg",
         price:"29.99",
         desc:"Wi-Fi Cameras for Home Security Outside with Starlight Night Vision, Facial Recognition, Built-in Siren, Cloud/Local Storage, Works with Alexa & Google Assistant",
     },
     {
+        name:"EKEN1080p",
+        Brand:"EKEN",
+        smart: true,
+        wireless: false,
+        img:"https://m.media-amazon.com/images/I/71fvtUX3MIL._AC_SX466_.jpg",
+        price:"26.95",
+        desc:"Intelligent Wired Video Doorbell Camera with Smart AI Human Detection, Protected Cloud Storage, Pre-Capture Capability, 2.4GHz Wi-Fi, Night Vision, Ring Chime Provided.",
+    },
+    {
+        name:"EKEN Solar Security Camera",
+        Brand:"EKEN",
+        smart: true,
+        wireless: true,
+        img:"https://m.media-amazon.com/images/I/61d4OIme8GL._AC_SX466_.jpg",
+        price:"29.97",
+        desc:"Wireless 1080P Surveillance Outdoor WiFi Bullet cam with 4 Spotlights, Color Night Vision, 2-Way Audio, PIR Motion Detection, Cloud Storage, Smart AI Human Detection",
+    },
+    {
         name:"",
         Brand:"",
+        smart:false, 
+        wireless:false,
         img:"",
         price:"",
         desc:"",
--- project/js/main.js
+++ project/js/main.js
@@ -1,15 +1,25 @@
 import '../css/style.css'
-//import{} from "";
+import{cameras} from"./Dom"
 
-function addCard(){
-    const card = `
-    <div class="box">
-        <h2>
-            ${names}
-        </h2>
+const DOMSelectors = {
+type: document.queryselector("cameras.brands"),
+smart: document.querySelector("cameras.smart")
+}
+
+cameras.forEach(element => DOMSelectors.container.insertAdjacentHTML(
+    "beforeend"
+
+    )
+);
+/* function addCard(){
+    const card = 
+    `<div class="box">
+        <h2>${names}</h2>
         <img src="${inputImage}" alt="user">
         <p>${desc}</p>
     </div>`
     DOMSelectors.container.insertAdjacentHTML("beforebegin", `${card}`)
-}
 
+} */
+
+//function card(arr)
